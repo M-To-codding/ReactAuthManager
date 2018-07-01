b81d95d4c4779e8606a6a273653ae4907a4018b3
im <marina.todorishina@mail.com>
2018-07-01 13:43:27 +0300

    Create menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Link} from "react-router-dom"
+
+import links from '../../resources/links.json';
 
 class Menu extends Component {
+
+  constructor(props) {
+    super(props);
+    this.links = links.links;
+    this.localS = [];
+  }
+
   render() {
+
+    const listOfLinks = this.links.map((val, index) =>
+      <li key={val.id}>
+        <Link to={val.link}>{val.name}</Link>
+      </li>
+    );
+
     return (
+
       <nav className="primary-menu">
-          <a href="#" className="link">
-            Menu work!
-          </a>
+        <ul>
+          {listOfLinks}
+        </ul>
       </nav>
+
     );
   }
 }
