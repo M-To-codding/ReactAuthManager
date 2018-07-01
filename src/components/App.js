cf87b759c822984974ef13918a2776804901ed45
im <marina.todorishina@mail.com>
2018-07-01 13:51:00 +0300

    Add routing,check localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,80 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Redirect} from 'react-router-dom'
+
 import './App.css';
 import Menu from "./menu/Menu";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import setAdmin from "../resources/setAdmin";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lsData: JSON.parse(localStorage.getItem('user' || '')),
+      sessionData: JSON.parse(sessionStorage.getItem('user' || '')),
+      userEmail: ''
+    };
+    this.component = {};
+
+    if(!this.state.lsData){
+      setAdmin();
+    }
+
+    console.log(this.state.lsData)
+  }
+
+  checkLocalStorage() {
+    this.findUser();
+
+    if (this.state.sessionData) {
+      this.component = Home;
+    } else {
+      this.component = Login;
+    }
+  }
+
   render() {
+
+    this.checkLocalStorage();
+
     return (
-      <div className="App">
-        <header className="App-header">
-          App work!
-          <Menu/>
-        </header>
-        <Home/>
-      </div>
+
+      <Router>
+
+        <div className="App">
+
+          {!this.localStData &&
+          <Redirect to="/login"/>
+          }
+
+          <header className="App-header">
+            <Menu/>
+          </header>
+
+          <main>
+            <Route
+              exact
+              path="/"
+              component={this.component}
+            />
+            <Route
+              exact
+              path="/login"
+              component={Login}
+            />
+            <Route
+              exact
+              path="/registration"
+              component={Registration}
+            />
+          </main>
+        </div>
+
+      </Router>
     );
   }
 }
