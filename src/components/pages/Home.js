fa03a9e43573bc54c9d4486fe47054a067239e1f
im <marina.todorishina@mail.com>
2018-07-01 13:52:59 +0300

    Add routing and nested pages to home component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,34 @@
-import React, { Component } from 'react';
-import './Contacts';
-import './Info';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+
 import Contacts from "./Contacts";
 import Info from "./Info";
+import Sidebar from "../sidebar/Sidebar";
 
 class Home extends Component {
   render() {
     return (
-      <section className="info">
-        <h1>Home work!</h1>
-        <Contacts/>
-        <Info/>
-      </section>
+      <Router>
+
+        <section className="home-page-data">
+          <h1>Home work!</h1>
+
+          <Sidebar/>
+
+          <Route
+            exact
+            path="/info"
+            component={Info}
+          />
+          <Route
+            exact
+            path="/contacts"
+            component={Contacts}
+          />
+        </section>
+
+      </Router>
+
     );
   }
 }
