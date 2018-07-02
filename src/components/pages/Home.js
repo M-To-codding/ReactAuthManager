66959d268b170fdaf8a7851a23a55f45782f6f6f
im <marina.todorishina@mail.com>
2018-07-02 04:28:52 +0300

    Add user checking in home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,32 +1,57 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Container from 'muicss/lib/react/container';
+import Row from 'muicss/lib/react/container';
 
-import Contacts from "./Contacts";
-import Info from "./Info";
-import Sidebar from "../sidebar/Sidebar";
+import Contacts from './Contacts';
+import Info from './Info';
+import Sidebar from '../sidebar/Sidebar';
+import sessionStHandlers from "../../helpers/sessionHandlers";
+import localStHandlers from "../../helpers/localStHandlers";
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      sessionData: sessionStHandlers.getItems('user'),
+      localStData: localStHandlers.getItems('user'),
+      currentUser: ''
+    }
+
+  }
+
+  getUserName(){
+    this.state.localStData.forEach((val)=>{
+      if(val.email === this.state.sessionData.Email){
+        this.state.currentUser = val.name;
+      }
+    })
+  }
+
   render() {
+    this.getUserName();
+
     return (
       <Router>
+        <Container>
+          <Row className="home-page-data">
+            <h1>Hello {this.state.currentUser}!</h1>
 
-        <section className="home-page-data">
-          <h1>Home work!</h1>
+            <Sidebar/>
 
-          <Sidebar/>
-
-          <Route
-            exact
-            path="/info"
-            component={Info}
-          />
-          <Route
-            exact
-            path="/contacts"
-            component={Contacts}
-          />
-        </section>
-
+            <Route
+              exact
+              path="/info"
+              component={Info}
+            />
+            <Route
+              exact
+              path="/contacts"
+              component={Contacts}
+            />
+          </Row>
+        </Container>
       </Router>
 
     );
