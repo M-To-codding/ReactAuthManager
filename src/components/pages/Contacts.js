0e728f606a23f74193b67b841823510fad3c6977
im <marina.todorishina@mail.com>
2018-07-02 04:30:16 +0300

    Simple fixes

--- src/components/pages/Contacts.js
+++ src/components/pages/Contacts.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 class Contacts extends Component {
   render() {
     return (
-      <section className="contacts">
-          <h2>Contacts work!</h2>
-      </section>
+      <article className="contacts">
+          <h2>Contacts</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi assumenda cumque eos eum ipsa similique
+          temporibus. At aut laudantium molestiae sunt tempore, voluptas. Consequuntur, doloribus minus nemo
+          reprehenderit repudiandae ullam.
+        </p>
+      </article>
     );
   }
 }
--- src/components/pages/Info.js
+++ src/components/pages/Info.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Info extends Component {
   render() {
     return (
-      <section className="info">
-        <h2>Info work!</h2>
-      </section>
+      <article className="info">
+        <h2>Info</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi assumenda cumque eos eum ipsa similique
+          temporibus. At aut laudantium molestiae sunt tempore, voluptas. Consequuntur, doloribus minus nemo
+          reprehenderit repudiandae ullam.
+        </p>
+      </article>
     );
   }
 }
