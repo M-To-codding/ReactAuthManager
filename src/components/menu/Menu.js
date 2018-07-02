ff9e00454ec2bcf3a46459211cd87b8390048ee1
im <marina.todorishina@mail.com>
2018-07-02 04:29:22 +0300

    Fix nav menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+import Appbar from 'muicss/lib/react/appbar';
 
 import links from '../../resources/links.json';
+import sessionStHandlers from "../../helpers/sessionHandlers";
 
 class Menu extends Component {
 
@@ -9,22 +11,38 @@ class Menu extends Component {
     super(props);
     this.links = links.links;
     this.localS = [];
+    this.state = {
+      sessionData: sessionStHandlers.getItems('user')
+    }
+  }
+
+  handleExitBtn(){
+    sessionStHandlers.removeUser('user');
+    window.location.href='/login';
   }
 
   render() {
 
     const listOfLinks = this.links.map((val, index) =>
-      <li key={val.id}>
-        <Link to={val.link}>{val.name}</Link>
+      <li key={val.id} className="nav-item">
+        <Link to={val.link} className="nav-link">{val.name}</Link>
       </li>
     );
 
     return (
 
       <nav className="primary-menu">
-        <ul>
+        <ul className="nav-items left-items">
           {listOfLinks}
         </ul>
+
+        {this.state.sessionData &&
+        <ul className="nav-items right-items">
+         <li className="exit-btn">
+           <a href="#" onClick={this.handleExitBtn} className="nav-link">Exit</a>
+         </li>
+        </ul>
+        }
       </nav>
 
     );
