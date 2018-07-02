1e43cc534ad64a7497e7cd8179420237a7aacc36
im <marina.todorishina@mail.com>
2018-07-02 04:29:41 +0300

    Create sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
+import './sidebar.css'
+
 class Sidebar extends Component {
   render() {
     return (
       <article className="sidebar">
-        <nav>
-          <ul>
-            <li>
+        <nav className="sidebar-nav">
+          <ul className="sidebar-tabs">
+            <li className="sidebar-tab">
               <Link to="/info">Info</Link>
             </li>
-            <li>
+            <li className="sidebar-tab">
               <Link to="/contacts">Contacts</Link>
             </li>
           </ul>
